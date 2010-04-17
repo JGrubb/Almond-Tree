21f8f84f43b11d7a6cd6e1bfd1053a98893f7bfe
JGrubb <therealjohnnygrubb@gmail.com>
2010-04-16 21:48:46 -0400

    getting started

---
+++ wp-content/themes/thesis_17/custom/custom_functions.php
@@ -0,0 +1,26 @@
+<?php
+
+// Using hooks is absolutely the smartest, most bulletproof way to implement things like plugins,
+// custom design elements, and ads. You can add your hook calls below, and they should take the 
+// following form:
+// add_action('thesis_hook_name', 'function_name');
+// The function you name above will run at the location of the specified hook. The example
+// hook below demonstrates how you can insert Thesis' default recent posts widget above
+// the content in Sidebar 1:
+// add_action('thesis_hook_before_sidebar_1', 'thesis_widget_recent_posts');
+
+// Delete this line, including the dashes to the left, and add your hooks in its place.
+
+/**
+ * function custom_bookmark_links() - outputs an HTML list of bookmarking links
+ * NOTE: This only works when called from inside the WordPress loop!
+ * SECOND NOTE: This is really just a sample function to show you how to use custom functions!
+ *
+ * @since 1.0
+ * @global object $post
+*/
+
+remove_action('thesis_hook_before_header', 'thesis_nav_menu');
+add_action('thesis_hook_after_header', 'thesis_nav_menu');
+//remove_action('thesis_hook_header', 'thesis_default_header');
+remove_action('thesis_hook_footer', 'thesis_hook_footer');
